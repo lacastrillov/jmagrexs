839f97ed6a63a4670925fda5cf2a05fcf6c7a64d
Luis Castrillo <lacastrillov@gmail.com>
2019-03-08 00:49:14 -0500

    Merge branch 'master' of https://github.com/lacastrillov/agogui
    # Conflicts:
    #	agogui-model/src/main/resources/META-INF/persistence.xml

--- src/main/java/com/lacv/jmagrexs/util/LetterCode.java
+++ src/main/java/com/lacv/jmagrexs/util/LetterCode.java
@@ -12,7 +12,7 @@ package com.lacv.jmagrexs.util;
 public class LetterCode {
 
     char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-    int N = 10;
+    int N = 14;
     long[] p26, acc;
 
     public LetterCode() {
@@ -29,12 +29,16 @@ public class LetterCode {
     }
 
     public String toString(long n) {
-        long num= n+1;
+        long num= n + 1;
         int digits = 0;
-        for (int i = 1; i < N; i++) {
-            if (num <= acc[i]) {
-                digits = i;
-                break;
+        if(num>acc[N-1]){
+            digits= 14;
+        }else{
+            for (int i = 1; i < N; i++) {
+                if (num <= acc[i]) {
+                    digits = i;
+                    break;
+                }
             }
         }
         long to_add = num - acc[digits - 1] - 1;
@@ -52,10 +56,26 @@ public class LetterCode {
         long result = 0;
         String rvn= new StringBuilder(n).reverse().toString();
 
-        for (int i = 0; i < rvn.length(); i++) {
+        for (int i = 0; i <rvn.length(); i++) {
             result += (rvn.charAt(i) - 'A') * p26[i];
         }
         return result + acc[rvn.length() - 1];
+    }
+    
+    public static void main(String args[]){
+        
+        LetterCode lc= new LetterCode();
+        
+        //System.out.println(">>\n"+(long)Math.pow(26, 15));
+        System.out.println(lc.toString((long)Math.pow(2, 63)-2));
+        
+        System.out.println(lc.toString((long)(1L<<63)-2));
+        //System.out.println(lc.toLong("JGZHJR"));
+        
+        for(int i=0; i< 10000; i++){
+            System.out.println(i +" :: "+ lc.toString(i));
+        }
+        
     }
 
 }
