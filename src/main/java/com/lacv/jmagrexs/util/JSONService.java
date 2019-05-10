f25a872dd8a2df8736df1f9aa6a7d9dbdc5564b3
e11001a <e11001a@LCOBGITSDMYZRN2.ena.us.experian.local>
2019-05-09 19:28:29 -0500

    Corrección ERROR formato de campo Date. Se elimina la hora del JSON.

--- src/main/java/com/lacv/jmagrexs/util/JSONService.java
+++ src/main/java/com/lacv/jmagrexs/util/JSONService.java
@@ -50,11 +50,11 @@ public class JSONService {
      */
     public static String objectToJson(Object obj) {
         String json= getGson().toJson(obj);
-        if(json.contains(" 00:00:00")){
-            json= json.replaceAll(" 00:00:00", "");
+        if(json.contains(" 00:00:00\"")){
+            json= json.replaceAll(" 00:00:00\"", "\"");
         }
-        if(json.contains(" 12:00:00 AM")){
-            json= json.replaceAll(" 12:00:00 AM", "");
+        if(json.contains(" 12:00:00 AM\"")){
+            json= json.replaceAll(" 12:00:00 AM\"", "\"");
         }
         return json;
     }
